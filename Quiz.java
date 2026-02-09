663096ec8a7c7bcc8bfedf6ada56222d71c97ee9
jhatcher-afk <jaxon.hatcher@stu.daviess.kyschools.us>
2026-02-09 20:32:12 +0000

    commit

--- Quiz.java
+++ Quiz.java
@@ -1,10 +1,33 @@
 public static String scrambleWord(String word){
   /* To be implemented in part (a) */
+  int count = 0;
+
+  for(word x: wordList)
+  {
+    if(word.length <= 1)
+    {
+      return word;
+    }
+    
+    
+  }
+  return scrambleWord(word);
+  
 
 }
 
 
 public static void scrambleOrRemove(List<String> wordList){
   /* To be implemented in part (b) */
+for(word x: wordList)
+{
+  if(scrambleWord(word))
+  {
+    wordList.remove(word);
+    
+    wordList.add(scrambleWord(word));
+  }
+
+}
 
 }
